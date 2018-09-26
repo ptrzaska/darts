ac782370029793918f65d66a6cc45a9edb063aea
przemek.trzaska@gmail.com <przemek.trzaska@gmail.com>
2018-09-26 20:24:23 +0200

    New webapi project, some junks propably

---
+++ DartsApp.API/Models/DayResult.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace DartsApp.API.Models
+{
+  public class DayResult
+  {
+    public int Id { get; set; }
+
+    public int PlayerId { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public bool DoubleOut { get; set; }
+
+    public bool DidNotFinished { get; set; }
+
+    public int BestScore { get; set; }
+
+    public int FinishRound { get; set; }
+
+    public int FinishThrow { get; set; }
+
+    public int TotalThrows { get; set; }
+  }
+}
--- DartsApp.API/Startup.cs
+++ DartsApp.API/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using DartsApp.API.Models;
 
 namespace DartsApp.API
 {
@@ -25,6 +20,7 @@ namespace DartsApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+      services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("AzureDB")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
